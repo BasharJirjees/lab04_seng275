4a79cea94346f61d2e31ed4f5cb8b7c538f20fa8
= <=>
2024-02-25 21:46:52 -0800

    Adding test cases for ComplexAdd

---
+++ src/main/java/com/mycompany/lab04/ComplexAdd.java
@@ -0,0 +1,13 @@
+package com.mycompany.lab04;
+
+public class ComplexAdd {
+
+	public int _ComplexAdd(int a, int b) {
+		if (a < 2) {
+			return (a + b) * -1;
+		} else {
+			return a + b;
+		}
+	}
+
+}
---
+++ src/test/java/testing/ComplexAddTest.java
@@ -0,0 +1,36 @@
+package testing;
+
+import com.mycompany.lab04.ComplexAdd;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ComplexAddTest {
+
+	@Test
+	public void _ComplexAddTest() {
+		ComplexAdd ca = new ComplexAdd();
+		
+		assertEquals(-1,ca._ComplexAdd(1, 0));
+		
+		assertEquals(0,ca._ComplexAdd(0, 0));
+		
+		assertEquals(1,ca._ComplexAdd(-1, 0));
+		
+		assertEquals(3,ca._ComplexAdd(3, 0));
+		
+		assertEquals(2,ca._ComplexAdd(3, -1));
+		
+		assertEquals(2,ca._ComplexAdd(-1, -1));
+		
+		assertEquals(0,ca._ComplexAdd(1, -1));
+		
+		assertEquals(0,ca._ComplexAdd(-1, 1));
+		
+        assertEquals(Integer.MAX_VALUE, ca._ComplexAdd(Integer.MAX_VALUE, 0));  // Boundary condition: a is maximum integer
+        assertEquals(Integer.MIN_VALUE, ca._ComplexAdd(Integer.MIN_VALUE, 0));
+        assertEquals(1, ca._ComplexAdd(Integer.MIN_VALUE, Integer.MAX_VALUE));
+	}
+
+}
